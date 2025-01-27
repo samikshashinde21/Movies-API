cd6149156574ce6a605ece5e2704c2a1bc7dbf99
samikshashinde21 <ssshinde.omk@gmail.com>
2025-01-27 22:54:19 +0530

    Home Component, Header Component, Carousel Styling

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
 import { Routes,Route } from 'react-router-dom';
+import Home from './components/home/Home';
+import Header from './components/header/Header';
 
 function App() {
 
@@ -32,7 +34,8 @@ function App() {
     <div className="App">
       
       <Routes>
-        <Route path='/' element={Layout}>
+        <Route path='/' element={<Layout/>}>
+        <Route path='/' element={<Home movies={movies}/>}></Route>
 
         </Route>
       </Routes>
