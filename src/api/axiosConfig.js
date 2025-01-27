bebcd3a1be9391a2c8209d38b63849efd1b8b663
samikshashinde21 <ssshinde.omk@gmail.com>
2025-01-27 19:55:52 +0530

    Route Mapping

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -1,6 +1,13 @@
-import axios from 'axios';
+/* import axios from 'axios';
 
 export default axios.create({
     baseURL:'http://9c96-103-106-239-104.ap.ngrok.io',
     headers: {"ngrok-skip-browser-warning": "true"}
+}); */
+
+
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: 'http://localhost:8080', 
 });
