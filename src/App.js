4b28edc4cef77094338351e1c2a1f51dd7078ff5
samikshashinde21 <ssshinde.omk@gmail.com>
2025-01-28 20:28:14 +0530

    Reviews and Review Form

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import Layout from './components/Layout';
 import { Routes,Route } from 'react-router-dom';
 import Home from './components/home/Home';
 import Header from './components/header/Header';
+import Trailer from './components/trailer/Trailer';
+import Reviews from './components/reviews/Reviews';
+import NotFound from './components/notFound/NotFound';
 
 function App() {
 
   const [movies, setMovies] = useState();
+  const [movie, setMovie] = useState();
+  const [reviews, setReviews] = useState();
+
   const getMovies = async () => {
     try {
       
@@ -24,6 +30,21 @@ function App() {
     }
   }
 
+  const getMovieData = async (movieId) => {
+    try {
+      const response = await api.get(`/api/v1/movies/${movieId}`);
+
+      const singleMovie = response.data;
+
+      setMovie(singleMovie);
+
+      setReviews(singleMovie.reviews);
+    }catch(error){
+      console.log(error);
+      
+    }
+  }
+
   useEffect(() => {
     getMovies();
   }, [])
@@ -32,12 +53,14 @@ function App() {
 
   return (
     <div className="App">
-      
+      <Header/>
       <Routes>
-        <Route path='/' element={<Layout/>}>
-        <Route path='/' element={<Home movies={movies}/>}></Route>
-
-        </Route>
+      <Route path="/" element={<Layout/>}>
+            <Route path="/" element={<Home movies={movies} />} ></Route>
+            <Route path="/Trailer/:ytTrailerId" element={<Trailer/>}></Route>
+            <Route path="/Reviews/:movieId" element ={<Reviews getMovieData = {getMovieData} movie={movie} reviews ={reviews} setReviews = {setReviews} />}></Route>
+            <Route path="*" element = {<NotFound/>}></Route> 
+          </Route>
       </Routes>
 
     </div>
